f49ebcf219f7d9d27c7006f04a0b9a4c05174851
IvanPasciucco <54645974+IvanPasciucco@users.noreply.github.com>
2025-12-12 03:16:22 -0300

    feat: serve index.html file

--- myApp.js
+++ myApp.js
@@ -4,11 +4,15 @@ let app = express();
 console.log("Hello World");
 
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+//   });
+
 app.get("/", function(req, res) {
-    res.send("Hello Express");
-  });
-
-
+  // __dirname nos da la ruta absoluta de la carpeta en el disco duro
+  const rutaAbsoluta = __dirname + "/views/index.html";
+  res.sendFile(rutaAbsoluta);
+});
 
 
 

c2d2c3030e2c0bc0f660ed0464f3894dc833e947
IvanPasciucco <54645974+IvanPasciucco@users.noreply.github.com>
2025-12-12 03:43:03 -0300

    feat: add chained middleware for time server

--- myApp.js
+++ myApp.js
@@ -28,6 +28,17 @@ app.get("/json", function(req, res) {
   res.json({ "message": message });
 });
 
+app.get(
+  "/now",
+  function(req, res, next) {
+    req.time = new Date().toString(); 
+    next(); 
+  },
+  function(req, res) {
+    res.json({ time: req.time }); 
+  }
+);
+
 
 
 
